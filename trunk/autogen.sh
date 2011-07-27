43cd10fdf82c64cec7730592320d20aa68d753c3
csilvers <csilvers@cb39b542-eb2e-2a05-867b-f1791dd5f601>
2011-07-27 00:39:12 +0000

    Forgot to update this file when I updated the rest for ctemplate-1.0rc1.

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -7,24 +7,23 @@
 # These are the files that this script might edit:
 #    aclocal.m4 configure Makefile.in src/config.h.in \
 #    depcomp config.guess config.sub install-sh missing mkinstalldirs \
-#    src/windows/ctemplate/* \
-#    ltmain.sh
 #
 # Here's a command you can run to see what files aclocal will import:
 #  aclocal -I ../autoconf --output=- | sed -n 's/^m4_include..\([^]]*\).*/\1/p'
+
+# Because libtoolize isn't in the hermetic build, autogen doesn't run it.
+# However, it should be run manually periodically to update these files:
+#    in .: ltmain.sh
+#    in m4: libtool.m4 ltoptions.m4 ltsugar.m4 ltversion.m4 lt~obsolete.m4
 
 set -ex
 rm -rf autom4te.cache
 
 aclocal --force -I m4
-grep -q LIBTOOL configure.ac && libtoolize -c -f
+#grep -q LIBTOOL configure.ac && libtoolize -c -f
 autoconf -f -W all,no-obsolete
 autoheader -f -W all
 automake -a -c -f -W all
-
-# This is specific to this project; it's the equivalent of manually running
-# 'configure' for a windows setup (mostly to process all the .in files).
-src/windows/preprocess.sh src
 
 rm -rf autom4te.cache
 exit 0
